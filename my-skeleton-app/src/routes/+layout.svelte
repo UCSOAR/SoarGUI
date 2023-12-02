e1ba3457d862d41707538b87d63c3315a827fcaf
Ethan Subasic <ethan.subasic@gmail.com>
2023-12-02 13:00:54 -0700

    Add Svelt Sidebar
    Adding sidebar, has links to different pages currently does not close,
    will add functionality to close and its planned to add minimizing to
    icon mode as well.

--- my-skeleton-app/src/routes/+layout.svelte
+++ my-skeleton-app/src/routes/+layout.svelte
@@ -9,12 +9,24 @@
 </script>
 
 <!-- App Shell -->
-<AppShell slotSidebarLeft="bg-surface-500/5 w-56 p-4">
+<AppShell slotSidebarLeft="bg-surface-500/5 w-0 lg:w-64">
 	<svelte:fragment slot="header">
 		<!-- App Bar -->
 		<AppBar>
 			<svelte:fragment slot="lead">
-				<strong class="text-xl uppercase">Skeleton</strong>
+				<div class="flex items-center">
+					<button class="lg:hidden btn btn-sm mr-4">
+							<span>
+									<svg viewBox="0 0 100 80" class="fill-token w-4 h-4">
+											<rect width="100" height="20" />
+											<rect y="30" width="100" height="20" />
+											<rect y="60" width="100" height="20" />
+									</svg>
+							</span>
+					</button>
+					<strong class="text-xl uppercase">Skeleton</strong>
+			</div>
+			
 			</svelte:fragment>
 			<svelte:fragment slot="trail">
 				<a
