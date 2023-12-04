9d8722bbf9ec6ba4d3e59b4a177b95f8ab1b92a4
Jesse Gerbrandt <jgerbrandt@live.com>
2023-12-03 18:15:43 -0700

    Adding App bar with logo dark mode slider and rocket state

--- my-skeleton-app/src/lib/navigation/navigation.svelte
+++ my-skeleton-app/src/lib/navigation/navigation.svelte
@@ -1,20 +1,18 @@
 <script>
-  import { getDrawerStore } from "@skeletonlabs/skeleton";
-  
-  const drawerStore = getDrawerStore();
+	import { getDrawerStore } from '@skeletonlabs/skeleton';
 
-  function drawerClose(): void {
-      drawerStore.close();
-  }
+	const drawerStore = getDrawerStore();
+
+	function drawerClose() {
+		drawerStore.close();
+	}
 </script>
-
-
 
 <nav class="list-nav p-4">
 	<ul>
 		<li><a href="/" on:click={drawerClose}>Homepage</a></li>
-    <li><a href="/about" on:click={drawerClose}>About</a></li>
-    <li><a href="/blog" on:click={drawerClose}>Blog</a></li>
-    <li><a href="/contact" on:click={drawerClose}>Contact</a></li>
+		<li><a href="/about" on:click={drawerClose}>About</a></li>
+		<li><a href="/blog" on:click={drawerClose}>Blog</a></li>
+		<li><a href="/contact" on:click={drawerClose}>Contact</a></li>
 	</ul>
 </nav>
--- my-skeleton-app/src/routes/+layout.svelte
+++ my-skeleton-app/src/routes/+layout.svelte
@@ -1,6 +1,16 @@
 <script lang="ts">
 	import '../app.postcss';
-	import { AppShell, AppBar, initializeStores, Drawer, getDrawerStore } from '@skeletonlabs/skeleton';
+	import {
+		AppShell,
+		AppBar,
+		initializeStores,
+		Drawer,
+		getDrawerStore,
+		LightSwitch,
+		getModalStore,
+		Modal
+	} from '@skeletonlabs/skeleton';
+	import type { ModalSettings, ModalComponent, ModalStore } from '@skeletonlabs/skeleton';
 	// Floating UI for Popups
 	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
 	import { storePopup } from '@skeletonlabs/skeleton';
@@ -14,69 +24,71 @@
 	const drawerStore = getDrawerStore();
 
 	function drawerOpen(): void {
-	drawerStore.open({});
+		drawerStore.open({});
 	}
+	const modalStore = getModalStore();
 
+	function modalConfirm(): void {
+		const modal: ModalSettings = {
+			type: 'confirm',
+			title: 'Please Confirm',
+			body: 'Are you sure you wish to proceed to the next state?',
+			response: (r: boolean) => console.log('response:', r)
+		};
+		modalStore.trigger(modal);
+	}
 </script>
 
-
 <Drawer>
-	<Navigation/>
+	<Navigation />
 </Drawer>
+
+<Modal />
 
 <!-- App Shell -->
 <AppShell slotSidebarLeft="bg-surface-500/5 w-0 lg:w-64">
 	<svelte:fragment slot="header">
 		<!-- App Bar -->
-		<AppBar>
+		<AppBar gridColumns="grid-cols-3" slotDefault="place-self-center" slotTrail="place-content-end">
 			<svelte:fragment slot="lead">
 				<div class="flex items-center">
 					<button class="lg:hidden btn btn-sm mr-4" on:click={drawerOpen}>
-							<span>
-									<svg viewBox="0 0 100 80" class="fill-token w-4 h-4">
-											<rect width="100" height="20" />
-											<rect y="30" width="100" height="20" />
-											<rect y="60" width="100" height="20" />
-									</svg>
-							</span>
+						<span>
+							<svg viewBox="0 0 100 80" class="fill-token w-4 h-4">
+								<rect width="100" height="20" />
+								<rect y="30" width="100" height="20" />
+								<rect y="60" width="100" height="20" />
+							</svg>
+						</span>
 					</button>
-					<strong class="text-xl uppercase">Skeleton</strong>
-			</div>
-			
+
+					<div class="flex items-center">
+						<img
+							src="/Copy of Clear.png"
+							alt="SOAR Logo"
+							class="mr-2"
+							style="width: 70px; height: 35px;"
+						/>
+					</div>
+				</div>
 			</svelte:fragment>
+
+			<svelte:fragment>
+				<button class="btn variant-filled-surface" on:click={modalConfirm}
+					>Current Rocket State: Abort</button
+				>
+			</svelte:fragment>
+
 			<svelte:fragment slot="trail">
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://discord.gg/EXqV7W8MtY"
-					target="_blank"
-					rel="noreferrer"
-				>
-					Discord
-				</a>
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://twitter.com/SkeletonUI"
-					target="_blank"
-					rel="noreferrer"
-				>
-					Twitter
-				</a>
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://github.com/skeletonlabs/skeleton"
-					target="_blank"
-					rel="noreferrer"
-				>
-					GitHub
-				</a>
+				<LightSwitch class="ml-auto" />
 			</svelte:fragment>
 		</AppBar>
 	</svelte:fragment>
 
 	<svelte:fragment slot="sidebarLeft">
-		<Navigation/>
+		<Navigation />
 	</svelte:fragment>
-	
+
 	<!-- Page Route Content -->
 	<slot />
 </AppShell>
